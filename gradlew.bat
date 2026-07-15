f0a5785997aecb51f5867fdd60c8da31c83e721c
Tayson <itaysondaniel@gmail.com>
2026-07-15 01:53:25 -0300

    build: harden Windows Gradle wrapper path assignments

--- gradlew.bat
+++ gradlew.bat
@@ -26,22 +26,22 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
+set "DIRNAME=%~dp0"
+if "%DIRNAME%"=="" set "DIRNAME=."
 @rem This is normally unused
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+set "APP_BASE_NAME=%~n0"
+set "APP_HOME=%DIRNAME%"
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+for %%i in ("%APP_HOME%") do set "APP_HOME=%%~fi"
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set "DEFAULT_JVM_OPTS=-Xmx64m -Xms64m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-set JAVA_EXE=java.exe
+set "JAVA_EXE=java.exe"
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
@@ -54,8 +54,8 @@ echo location of your Java installation. 1>&2
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set "JAVA_HOME=%JAVA_HOME:"=%"
+set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
 
 if exist "%JAVA_EXE%" goto execute
 
@@ -70,7 +70,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set "CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar"
 
 
 @rem Execute Gradle
@@ -83,8 +83,8 @@ if %ERRORLEVEL% equ 0 goto mainEnd
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-set EXIT_CODE=%ERRORLEVEL%
-if %EXIT_CODE% equ 0 set EXIT_CODE=1
+set "EXIT_CODE=%ERRORLEVEL%"
+if %EXIT_CODE% equ 0 set "EXIT_CODE=1"
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
 
