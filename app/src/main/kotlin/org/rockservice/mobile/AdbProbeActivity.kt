a614fd661e0cc73cfd27449fcd87ffda5e5c7d99
Tayson <itaysondaniel@gmail.com>
2026-07-16 05:02:31 -0300

    feat(app): add ADB probe screen shell

---
+++ app/src/main/kotlin/org/rockservice/mobile/AdbProbeActivity.kt
@@ -0,0 +1,10 @@
+package org.rockservice.mobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class AdbProbeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
